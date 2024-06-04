5dd28a6b2227de0ec09fdc89cc8325a94a15d42b
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-06-04 13:37:35 +0530

    Update RecipeServiceTest.java

--- src/test/java/com/yaksha/training/recipe/service/RecipeServiceTest.java
+++ src/test/java/com/yaksha/training/recipe/service/RecipeServiceTest.java
@@ -122,7 +122,7 @@ public class RecipeServiceTest {
 	}
 
 	@Test
-	public void testControllerUpdateStatus() throws Exception {
+	public void testServiceUpdateStatus() throws Exception {
 		Recipe recipe = getRecipe();
 		String status = randomBoolean() ? RecipeStatus.APPROVED.toString() : RecipeStatus.REJECTED.toString();
 
